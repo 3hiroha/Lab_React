563290056e1bd5acc05442278c35a2b217c4f5fb
Shiroha <pavadol2@gmail.com>
2023-03-16 10:50:14 +0700

    First commit.

---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import "./Profile.css";
+function Profile(){
+    return(
+        <div className="layout">
+            <img src="assets/img1.jpg" alt="Profile"/>
+            <h1 className="name">test 1 2 3</h1>
+            <hr></hr>
+        </div>
+    )
+}
+
+export default Profile;
